d1052e10962c4ae0785d4c6af70908db5b2044c3
Peter Gao <petergao@berkeley.edu>
2013-12-09 14:45:10 -0800

    fixed partitionawareunion

--- core/src/main/scala/org/apache/spark/rdd/PartitionerAwareUnionRDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/PartitionerAwareUnionRDD.scala
@@ -24,6 +24,10 @@ class PartitionerAwareUnionRDD[T: ClassManifest](
   require(rdds.flatMap(_.partitioner).toSet.size == 1,
     "Parent RDDs do not have the same partitioner: " + rdds.flatMap(_.partitioner))
 
+  require(rdds.map(_.partitions.length).toSet.size == 1,
+    "Parent RDDs do not have the same num of partitions: " + rdds.map(_.partitions.length) + rdds)
+
+
   override val partitioner = rdds.head.partitioner
 
   override def getPartitions: Array[Partition] = {
@@ -32,6 +36,11 @@ class PartitionerAwareUnionRDD[T: ClassManifest](
       val parentPartitions = rdds.map(_.partitions(index)).toArray
       new PartitionerAwareUnionRDDPartition(index, parentPartitions)
     }).toArray
+  }
+
+  override def clearDependencies() {
+    super.clearDependencies()
+    rdds = null
   }
 
   // Get the location where most of the partitions of parent RDDs are located
@@ -55,6 +64,10 @@ class PartitionerAwareUnionRDD[T: ClassManifest](
 
   override def compute(s: Partition, context: TaskContext): Iterator[T] = {
     val parentPartitions = s.asInstanceOf[PartitionerAwareUnionRDDPartition].partitions
+//    val iters = rdds.zip(parentPartitions).map {
+//      case (rdd, p) => rdd.iterator(p, context)
+//    }
+//    new CombinedIterator(iters)
     rdds.zip(parentPartitions).iterator.flatMap {
       case (rdd, p) => rdd.iterator(p, context)
     }
@@ -63,6 +76,38 @@ class PartitionerAwareUnionRDD[T: ClassManifest](
   // gets the *current* preferred locations from the DAGScheduler (as opposed to the static ones)
   private def currPrefLocs(rdd: RDD[_], part: Partition): Seq[String] = {
     rdd.context.getPreferredLocs(rdd, part.index).map(tl => tl.host)
+  }
+
+
+  class CombinedIterator[T: ClassManifest](iters : Seq[Iterator[T]]) extends Iterator[T]{
+    var remainingSeqs = iters
+
+    var currentIter : Iterator[T]  = Iterator()
+
+    def hasNextRecursive(seqs : Seq[Iterator[T]]) : Boolean = {
+      if(seqs.isEmpty)
+        false
+      else if(seqs.head.hasNext)
+        true
+      else
+        hasNextRecursive(seqs.tail)
+    }
+
+    override def hasNext = {
+      currentIter.hasNext || hasNextRecursive(remainingSeqs)
+    }
+
+    override def next : T = {
+      if(currentIter.hasNext)
+        currentIter.next()
+      else{
+        if(remainingSeqs.isEmpty)
+          throw new NoSuchElementException("next on empty iterator")
+        currentIter = remainingSeqs.head
+        remainingSeqs = remainingSeqs.tail
+        this.next
+      }
+    }
   }
 
 }
